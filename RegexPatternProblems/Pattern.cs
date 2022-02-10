f690a5fc4e01465f836c16802303dfb98ddd6b9c
aamir0143 <mdaamirreza00@gmail.com>
2022-02-10 16:50:56 +0530

    [Aamir19] Add : enter a valid first name

---
+++ RegexPatternProblems/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPatternProblems
+{
+    //Class name.
+   public class Pattern 
+   {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$"; //Minimum 3 Characters
+
+        //Method For Validate First Name.
+        public bool validateFirstName(string fname) 
+        {
+            return Regex.IsMatch(fname, FIRST_NAME_REGEX);
+        }
+   }
+}
--- RegexPatternProblems/Program.cs
+++ RegexPatternProblems/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To User Registration Problems==========");
-Console.ReadLine();
+using System;
+namespace RegexPatternProblems
+{
+    class Program
+    {
+        public static string instr = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To User Registration Problems==========");
+            Pattern program = new Pattern();
+            Console.WriteLine("-->Please Choose To Which Program Is Executed<--");
+            Console.WriteLine("1.validateFirstName \n2.Exit");
+            Console.Write("Select Above Number:-> ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(program.validateFirstName(instr));
+                    break;
+                case 2:
+                default:
+                    Console.WriteLine("Please Select Correct Number");
+                    break;  
+            }
+        }
+    }
+}
